1c1a839e88f13869e692777f4ec71aeb8f014489
ssalinian <agussalim@barmail.ch>
2026-03-07 22:27:07 +0700

    update file

--- worker.js
+++ worker.js
@@ -1,2 +1,8 @@
-# push 03/07/2026 22:16:47
-export default{async fetch(){return new Response("scheduler",{headers:{"content-type":"text/plain"}})}}
+// push 03/07/2026 22:26:42
+export default {
+  async fetch(){
+    return new Response("scheduler", {
+      headers: {"content-type":"text/plain"}
+    })
+  }
+}
